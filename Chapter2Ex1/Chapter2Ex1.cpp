e8f3162631a90171f605f6cd59b2e50b803771dd
mjgoldstein68 <mjgoldstein68@gmail.com>
2025-07-24 10:52:18 -0400

    First iteration - Notice requirements do not specify display of the answer

---
+++ Chapter2Ex1/Chapter2Ex1.cpp
@@ -0,0 +1,22 @@
+/*
+Project Name: Chapter 2 Exercise 1 - Sum of two numbers
+File Name: Chapter2Ex1.cpp
+Programmer: Mark Goldstein
+Date:07/2025
+Requirements:
+    Write a program that stores the integers 50 and 100 in variables, 
+    and stores the sum of these two in a variable named total.
+
+
+*/
+
+#include <iostream>
+
+int main()
+{
+    int number1 = 50;
+    int number2 = 100;
+    int total = number1 + number2;
+
+}
+
